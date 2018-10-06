997e45775925a06f16481afae6e68ec238e67c7b
Erik JA <OgJAkFy8@users.noreply.github.com>
2018-10-06 14:36:38 -0400

    Updated param statement
    added another couple changes to the param statement.  used env:homedrive

--- Export-Backups.ps1
+++ Export-Backups.ps1
@@ -1,23 +1,43 @@
 <#
-.SYNOPSIS
+    .SYNOPSIS
     Export Backup Files to Share Drive and Delete Old Files on Share
-.DESCRIPTION
+    .DESCRIPTION
     We needed to export SQL backups to a share along with deleting old files. 
     This was the workaround. Make sure you update the file paths on lines 18, 38, 40, and 43!
-.NOTES
+    .NOTES
     File Name      : Export-DBBackups.ps1
     Author         : Alex Flom (https://github.com/afflom)
     Prerequisite   : PowerShell V2 over Vista and upper.
     Copyright 2018 - Alex Flom
-.LINK
+    .LINK
     Script posted over:
     https://github.com/afflom/quick-scripts
 #>
 
+  param
+  (
+    [Parameter(HelpMessage='Prefix of file or log name')]
+    [alias('Daysback')]
+    [int]$daysback = -14,
+
+    [Parameter(HelpMessage='Extention of file.  txt, csv, log')]
+    [alias('Extension')]
+    [string]$tailNAME ='bak',
+    [Parameter(HelpMessage='Formatting Choice 1 to 4')]
+    [alias('Choice')]
+    [ValidateRange(1,4)]
+    [int]$StampFormat,
+    [Parameter(Position = 0)] [string]$SourceFolder = "$env:HOMEDRIVE\temp\BackupSource",
+    [Parameter(Position = 1)] [string]$DestinationFolder = "$env:HOMEDRIVE\temp\BackupDestination"
+  )
+
 #Get Backup Directories
 $folderList = (Get-ChildItem -Path '.\Documents\Powershell Scripts\BackupSandbox\').fullname
+
 #Declare obsolete backups
-$old = Get-date.addDays(-14)
+$old = Get-date.addDays($daysback)
+
+
 #Loop through the directories
 foreach($folder in $folderList){
     #Find Backup in folder
@@ -37,8 +57,8 @@ foreach($folder in $folderList){
     #Add to backup log
     $outputList | Export-Csv -NoTypeInformation -Path '.\Documents\Powershell Scripts\backups.csv' -Delimiter ','  -Append
     #Export backup to share
-    Move-Item -Path $folder\*.bak -Destination $folder\..\..\movefolder\
+    Move-Item -Path $SourceFolder\*.$tailNAME -Destination $DestinationFolder\..\..\movefolder\
 }
 #Delete old backups
-Get-ChildItem -Path $folder\..\..\movefolder\ | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $old } | Remove-Item -Force
+Get-ChildItem -Path $SourceFolder\..\..\movefolder\ | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $old } | Remove-Item -Force
 
