1d0524a7299635541ae9d72a5bdeb58001b763a7
Erik Arnesen <earnesen@ymail.com>
2018-10-25 19:02:05 -0400

    Clean-up changes
    Changes clean up code.  Also address issue.

--- DCDiag.ps1
+++ DCDiag.ps1
@@ -1,18 +1,58 @@
 ﻿
+<#
+.SYNOPSIS
+    Runs DCDIAG against all the DC in the domain.  Sets up a PSSession and runs dcdiag against that server's session.  There is no input needed, because it searches AD for all domain controllers.
 
-$StampDateTime = Get-Date -Uformat %Y%m%d
-$OutputReportPath = "$env:HOMEDRIVE\Temp\"
-$OutPutReportUNC = '\\UNCFileshare'
-$getForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest() 
+.EXAMPLE Get-DcDiag
+    Get-DcDiag
 
-$DCServers = $getForest.domains | ForEach-Object {$_.DomainControllers} | ForEach-Object {$_.Name} 
+.INPUT
+    None
 
+.OUTPUT
+    DC Diag report to file
+#>
+
+# Load the time stamp module
+& ((Split-Path $MyInvocation.InvocationName) + "\Create-LogFileNameWithTimeDateStamp.ps1")
+
+
+# Set variables
+# $StampDateTime = Get-Date -Uformat %Y%m%d
+$OutputReportPath = "$env:HOMEDRIVE\temp\"
+$OutPutReportUNC = '\\UNCFileshare\'
+
+# Get Domain Contollers
+$getForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest()
+$DCServers = $getForest.domains | ForEach-Object {$_.DomainControllers}
 
 foreach($DomainController in $DCServers){
-   $DcDiagReportOutput = ('{0}-DCDiag-{1}.txt' -f $DomainController, $StampDateTime)
-   
-   dcdiag /f: ('{0}+{1}' -f $OutputReportPath, $DcDiagReportOutput)
+
+   # Create the PS session
+   $session = New-PSSession -ComputerName $DomainController.Name
+
+   # create Output report filename
+   # $DcDiagReportOutput = ('{0}-DCDiag-{1}.txt' -f $DomainController.Name, $StampDateTime)
+   $DcDiagReportOutput = New-TimedStampFileName -baseNAME ($DomainController.Name+'-DCDiag') -Extension txt -StampFormat 2
+
+   # Use the invoke command to create a variable to run the dcdiag
+   $dcdiag  = Invoke-Command -Session $session -Command  { dcdiag }
+
+   # Run the DC Diag on the remote machine as defined in the Session and dcdiag lines then pass it to the outputfile.
+   $dcdiag | ('{0}+{1}' -f $OutputReportPath, $DcDiagReportOutput)
+
+   # This may be able to be removed later
    Copy-Item -Path ('{0}+{1}' -f $OutputReportPath, $DcDiagReportOutput) -Destination $OutPutReportUNC
-   exit
 
 }
+
+<#
+Future features
+Remove copy item statement
+Put in error catching (try / catch)
+Rename using proper Verb-Noun format.  ex : Get-DCDiagStatus
+
+Issues
+Untested
+
+#>
