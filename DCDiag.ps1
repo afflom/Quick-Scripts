7c9589c2a606b26cd1c97952a2052e827a020cc8
Erik Arnesen <earnesen@ymail.com>
2018-10-25 06:47:35 -0400

    Update DCDiag.ps1
    Changed the date time stamp to a variable per the "issue" .  Also, started to make it able to be run remotely, but realized after making the loop, I would be unable to test and probably have to make a pssession

--- DCDiag.ps1
+++ DCDiag.ps1
@@ -1,3 +1,18 @@
-﻿dcdiag /f:<Path>\$($env:COMPUTERNAME)-DCDiag-$(Get-Date -Uformat %Y%m%d).txt
-Copy-Item "<Same Path>\$($env:COMPUTERNAME)-DCDiag-$(Get-Date -Uformat %Y%m%d).txt" -Destination '<UNC Path>'
-exit
+﻿
+
+$StampDateTime = Get-Date -Uformat %Y%m%d
+$OutputReportPath = "$env:HOMEDRIVE\Temp\"
+$OutPutReportUNC = '\\UNCFileshare'
+$getForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest() 
+
+$DCServers = $getForest.domains | ForEach-Object {$_.DomainControllers} | ForEach-Object {$_.Name} 
+
+
+foreach($DomainController in $DCServers){
+   $DcDiagReportOutput = ('{0}-DCDiag-{1}.txt' -f $DomainController, $StampDateTime)
+   
+   dcdiag /f: ('{0}+{1}' -f $OutputReportPath, $DcDiagReportOutput)
+   Copy-Item -Path ('{0}+{1}' -f $OutputReportPath, $DcDiagReportOutput) -Destination $OutPutReportUNC
+   exit
+
+}
