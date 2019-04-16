cf7081c51eb98eeae263614292ba15e87917a29f
Erik Arnesen <earnesen@ymail.com>
2019-04-15 20:09:41 -0400

    Quick updates

--- AllForestAccounts.ps1
+++ AllForestAccounts.ps1
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿
+
 function Get-AllForestUserAccountGroupMemberships
 {
   <#
@@ -31,46 +30,44 @@ function Get-AllForestUserAccountGroupMemberships
     
   param
   (
-    [Parameter(Mandatory,HelpMessage='Name of File. The Date stamp will be added by the program.')]
+    [Parameter(Mandatory,HelpMessage = 'Name of File. The Date stamp will be added by the program.')]
     [string]$OutputFileNamePrefix
   )
     
-  Function Get-FileName{
-      
+  Function Get-FileName
+  {
     param
     (
-      [Parameter(Mandatory,HelpMessage='Prefix of filename')]
+      [Parameter(Mandatory,HelpMessage = 'Prefix of filename')]
       [string]
       $NamePrefix
     )
-    $timeStamp = Get-Date -uformat %Y%m%d
+    $timeStamp = Get-Date -UFormat %Y%m%d
     return  $NamePrefix + '_' + $timeStamp + '.csv'
   }
     
   $OutputFileName = Get-FileName -OutputFileNamePrefix 
-  ﻿
+
   # Get list of Domains in the Forest
   $domains        = (Get-ADForest).domains
     
   # Loop through the domains
-  Foreach ($domain in $domains){
-      
+  Foreach ($domain in $domains)
+  {
     $AllUserGUIDS = ((Get-ADUser -Filter * -Server $domain).objectGUID)
       
-    foreach($UserGUID in $AllUserGUIDS){
-        
+    foreach($UserGUID in $AllUserGUIDS)
+    {
       $accountName = (Get-ADUser -Identity $UserGUID -Server $domain).distinguishedname
         
-      $membership  = (Get-ADPrincipalGroupMembership -Identity $UserGUID -Server $Domain).distinguishedname
+      $membership  = (Get-ADPrincipalGroupMembership -Identity $UserGUID -Server $domain).distinguishedname
         
       Write-Output -InputObject $accountName
         
-      foreach($group in $membership){
-          
+      foreach($group in $membership)
+      {
         $OutputList = New-Object -TypeName PSObject -Property @{
-            
           AccountName = $accountName
-            
           Groups      = $group
         }
         $OutputList | Export-Csv -NoTypeInformation -Path $OutputFileName -Delimiter ',' -Append
@@ -80,31 +77,6 @@ function Get-AllForestUserAccountGroupMemberships
 }
 
 
-
-# SIG # Begin signature block
-  # MIID7QYJKoZIhvcNAQcCoIID3jCCA9oCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-  # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-  # AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU2vFGYfw23LVI/yRX4lRa+uGA
-  # QgigggINMIICCTCCAXagAwIBAgIQyWSKL3Rtw7JMh5kRI2JlijAJBgUrDgMCHQUA
-  # MBYxFDASBgNVBAMTC0VyaWtBcm5lc2VuMB4XDTE3MTIyOTA1MDU1NVoXDTM5MTIz
-  # MTIzNTk1OVowFjEUMBIGA1UEAxMLRXJpa0FybmVzZW4wgZ8wDQYJKoZIhvcNAQEB
-  # BQADgY0AMIGJAoGBAKYEBA0nxXibNWtrLb8GZ/mDFF6I7tG4am2hs2Z7NHYcJPwY
-  # CxCw5v9xTbCiiVcPvpBl7Vr4I2eR/ZF5GN88XzJNAeELbJHJdfcCvhgNLK/F4DFp
-  # kvf2qUb6l/ayLvpBBg6lcFskhKG1vbEz+uNrg4se8pxecJ24Ln3IrxfR2o+BAgMB
-  # AAGjYDBeMBMGA1UdJQQMMAoGCCsGAQUFBwMDMEcGA1UdAQRAMD6AEMry1NzZravR
-  # UsYVhyFVVoyhGDAWMRQwEgYDVQQDEwtFcmlrQXJuZXNlboIQyWSKL3Rtw7JMh5kR
-  # I2JlijAJBgUrDgMCHQUAA4GBAF9beeNarhSMJBRL5idYsFZCvMNeLpr3n9fjauAC
-  # CDB6C+V3PQOvHXXxUqYmzZpkOPpu38TCZvBuBUchvqKRmhKARANLQt0gKBo8nf4b
-  # OXpOjdXnLeI2t8SSFRltmhw8TiZEpZR1lCq9123A3LDFN94g7I7DYxY1Kp5FCBds
-  # fJ/uMYIBSjCCAUYCAQEwKjAWMRQwEgYDVQQDEwtFcmlrQXJuZXNlbgIQyWSKL3Rt
-  # w7JMh5kRI2JlijAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKA
-  # ADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYK
-  # KwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUvby27ewAvTFZNxKCHDDz+fJ5U6Qw
-  # DQYJKoZIhvcNAQEBBQAEgYAST7tlGYj0nyE2ci/mBEOfwIAasJT8Qgp01Ziwiiin
-  # DX0+JbTNmzt/B0gPKzACA+LphUQ+K8z2wE9acuSAo5D5+geEmGieCJT+2FOOpjpD
-  # t+rJPuGGOXF/ncwV7bS2Op3oIhm5k4Is9nLxpJPXRxwlefjeqgpOT+t2lkcCyWFa
-  # Kw==
-# SIG # End signature block
 =======
 function Get-AllForestUserAccountGroupMemberships
 {
@@ -137,46 +109,43 @@ function Get-AllForestUserAccountGroupMemberships
     
   param
   (
-    [Parameter(Mandatory,HelpMessage='Name of File. The Date stamp will be added by the program.')]
+    [Parameter(Mandatory,HelpMessage = 'Name of File. The Date stamp will be added by the program.')]
     [string]$OutputFileNamePrefix
   )
     
-  Function Get-FileName{
-      
+  Function Get-FileName
+  {
     param
     (
-      [Parameter(Mandatory,HelpMessage='Prefix of filename')]
+      [Parameter(Mandatory,HelpMessage = 'Prefix of filename')]
       [string]
       $NamePrefix
     )
-    $timeStamp = Get-Date -uformat %Y%m%d
+    $timeStamp = Get-Date -UFormat %Y%m%d
     return  $NamePrefix + '_' + $timeStamp + '.csv'
   }
     
   $OutputFileName = Get-FileName -OutputFileNamePrefix 
-  ﻿
   # Get list of Domains in the Forest
   $domains        = (Get-ADForest).domains
     
   # Loop through the domains
-  Foreach ($domain in $domains){
-      
+  Foreach ($domain in $domains)
+  {
     $AllUserGUIDS = ((Get-ADUser -Filter * -Server $domain).objectGUID)
       
-    foreach($UserGUID in $AllUserGUIDS){
-        
+    foreach($UserGUID in $AllUserGUIDS)
+    {
       $accountName = (Get-ADUser -Identity $UserGUID -Server $domain).distinguishedname
         
-      $membership  = (Get-ADPrincipalGroupMembership -Identity $UserGUID -Server $Domain).distinguishedname
+      $membership  = (Get-ADPrincipalGroupMembership -Identity $UserGUID -Server $domain).distinguishedname
         
       Write-Output -InputObject $accountName
         
-      foreach($group in $membership){
-          
+      foreach($group in $membership)
+      {
         $OutputList = [pscustomobject]@{
-            
           AccountName = $accountName
-            
           Groups      = $group
         }
         $OutputList | Export-Csv -NoTypeInformation -Path $OutputFileName -Delimiter ',' -Append
@@ -184,6 +153,3 @@ function Get-AllForestUserAccountGroupMemberships
     }
   }
 }
-
-
->>>>>>> patch-2
--- AllForestComputers.ps1
+++ AllForestComputers.ps1
@@ -1,26 +1,43 @@
-﻿
-$domains = (Get-ADForest).domains
+﻿#requires -Version 3.0
+#requires -Module ActiveDirectory
+#requires -RunAsAdministrator 
 
+Function Get-ForestComputers
+{
+  <#
+      .SYNOPSIS
+      Returns a CSV of all of the computers in all of the Forests.
 
-Foreach ($domain in $domains){
+      .EXAMPLE
+      Get-ForestComputers
+      Finds the AD Forests, then searches each for computers and outputs the list to a CSV
 
-    $fqdn = $domain
+      .OUTPUTS
+      Sends the report to the users desktop.
+      All_Forest_Computers_DateStamp.csv a file with a date.
+  #>
+  
+  $DateStamp = Get-Date -UFormat %Y%m%d  
+  $OutputFileName = "C:\Users\Erik.Arnesen\Desktop\All_Forest_Computers_$DateStamp.csv"
 
+  $domains = (Get-ADForest).domains
+  Write-Verbose -Message ('Domains = {0}' -f $domains)
+  Foreach ($domain in $domains)
+  {
+    Write-Verbose -Message ('This Domain = {0}' -f $domain)
+ 
     $domain = ((Get-ADComputer -Filter * -Server $domain).name)
-
-
-    foreach($computer in $domain){
-
-        
-        [pscustomobject]@{
-
-            ComputerName = $computer
-
-            DomainName = $domain
-
-        } | Export-Csv -NoTypeInformation All_Forest_Computers_$(Get-Date -UFormat %Y%m%d).csv -Append
+    foreach($computer in $domain)
+    {
+      Write-Verbose -Message ('This Computer = {0}' -f $computer)
+      [pscustomobject]@{
+        ComputerName = $computer
+        DomainName   = $domain
+      } | Export-Csv -NoTypeInformation -Path $OutputFileName -Append
     }
+  }
+}
 
- }
-
+#Start Script
+Get-ForestComputers
 
